fdf8237f748f1560851350831fe2abc5d945f0b9
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-09-01 20:37:46 +0200

    Created DAO implementation

---
+++ src/com/luv2code/springdemo/dao/CuustomerDAOImpl.java
@@ -0,0 +1,35 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CuustomerDAOImpl implements CustomerDAO {
+
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<Customer> theQuery = currentSession.createQuery("from Customer",Customer.class);
+		
+		List<Customer> customers = theQuery.getResultList();
+		
+		return customers;
+		
+	}
+
+}
